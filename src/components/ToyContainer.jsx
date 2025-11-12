838ec10675eb3d6796ca3dbd8e0efbe8e7639ee7
David N <paperish@icloud.com>
2025-11-12 03:00:15 -0700

    finished display toys feature

--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 import ToyCard from "./ToyCard";
+import { useState, useEffect } from "react";
 
 function ToyContainer() {
+  const [toys, setToys] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/toys")
+      .then((response) => response.json())
+      .then((data) => setToys(data))
+      .catch((error) => console.error('Error getting toys', error));
+  }, []);
+
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">
+      {toys.map((toy) => (
+        <ToyCard key={toy.id} toy={toy} />
+      ))}
+    </div>
   );
 }
 
